45ad87bfc3b6b6cf1662110708fe583b98d81ae6
Țeculescu Octavian-Alexandru <octav_teculescu@yahoo.com>
2020-05-25 21:33:31 +0300

    Corrected jwt extraction name

--- be/security/jwt.js
+++ be/security/jwt.js
@@ -19,9 +19,9 @@ const authorize = async (req, res, next) => {
         }
         const token = req.headers.authorization.split(' ')[1];
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const decodedJwt = jwt.verify(token, process.env.JWT_SECRET_KEY);
         req.state = {
-            decoded,
+            decodedJwt,
             ...req.state
         };
 
